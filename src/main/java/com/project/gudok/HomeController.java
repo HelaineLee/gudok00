ef7a1da43b66ab9460215699a3724df96f442653
zzyeah0 <65704669+zzyeah0@users.noreply.github.com>
2020-06-10 17:33:14 +0900

    세번째 커밋~~!

---
+++ src/main/java/com/project/gudok/HomeController.java
@@ -0,0 +1,21 @@
+package com.project.gudok;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "hello";
+    }
+
+}
